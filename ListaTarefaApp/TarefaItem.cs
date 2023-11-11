04e7c81d64fe92862f4ef712cfa067685de98f5b
Aluno <aluno@etec.sp.gov.br>
2023-11-11 11:42:47 -0300

    firt commit

---
+++ ListaTarefaApp/TarefaItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ListaTarefaApp
+{
+    public class TarefaItem
+    {
+        public Guid Id { get; set; }
+        public string Descricao { get; set; }
+        public bool FlagFeito { get; set; }
+        public DateTime DataCriacao { get; set; }       
+    }
+}
